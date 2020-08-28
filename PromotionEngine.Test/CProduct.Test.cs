0a7a76cfd6b220fd55eb515f21f4ca755f9c464b
Nischal <vkg.arya@gmail.com>
2020-08-28 16:25:46 +0530

    13th commit - Added one test case for Product-'C' and it is failing - TDD approach

---
+++ PromotionEngine.Test/CProduct.Test.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PromotionEngine.Services;
+
+namespace PromotionEngine.Test
+{
+    [TestClass]
+    public class CProductTest
+    {
+        [TestMethod]
+        public void GetPrice_ProductCount_3_Success()
+        {
+            CProduct cProduct = new CProduct
+            {
+                ProductsCount = 3
+            };
+
+            decimal finalPrice = cProduct.GetPrice();
+            Assert.AreEqual(60, finalPrice);
+        }
+    }
+}
