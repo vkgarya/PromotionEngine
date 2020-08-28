390b2897c7ddf544b47c07803e24014dcd340630
Nischal <vkg.arya@gmail.com>
2020-08-28 16:57:16 +0530

    27th commit - This application is ready for extension and adding Product - 'E' with price 100 and having discount of 30%. Putting minimum code for Product E so that test case can be passed - TDD approach

---
+++ PromotionEngine/Services/EProduct.cs
@@ -0,0 +1,14 @@
+﻿using PromotionEngine.Interfaces;
+
+namespace PromotionEngine.Services
+{
+    /// <summary>
+    /// EProduct
+    /// Rule - Active Promotions: 30% off on E
+    /// </summary>
+    public class EProduct : IPromotion
+    {
+        public int ProductsCount { get; set; }
+        public decimal GetPrice() => ProductsCount * 100 * (100 - 30) / 100;
+    }
+}
