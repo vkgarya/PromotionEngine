3a9f38adcc7e0695b86daeef2df0eb67f62d9a3f
Nischal <vkg.arya@gmail.com>
2020-08-28 16:48:06 +0530

    23rd commit - Added one failure test case for Product - 'C' - Quantity-2 and Product - 'D' - Quantity-1 combination

--- PromotionEngine.Test/CDProduct.Test.cs
+++ PromotionEngine.Test/CDProduct.Test.cs
@@ -55,5 +55,21 @@ namespace PromotionEngine.Test
             decimal finalPrice = cdProduct.GetPrice();
             Assert.AreEqual(50, finalPrice);
         }
+
+        [TestMethod]
+        public void GetPrice_CProductCount_2_DProductCount_1_Failure()
+        {
+            CDProduct cdProduct = new CDProduct
+            {
+                Products = new List<Product>
+                {
+                    new Product{ProductCount = 2, ProductName = "C" },
+                    new Product{ProductCount = 1, ProductName = "D" }
+                }
+            };
+
+            decimal finalPrice = cdProduct.GetPrice();
+            Assert.AreNotEqual(55, finalPrice);
+        }
     }
 }
