3372e19b2fc80963e4eb6baee701f7b1849e7cae
Nischal <vkg.arya@gmail.com>
2020-08-28 17:25:52 +0530

    32nd commit - Added failure test case with Product-ProductCount: A-1, B-1, C-1 in ProgramTest.cs

--- PromotionEngine.Test/Program.Test.cs
+++ PromotionEngine.Test/Program.Test.cs
@@ -8,6 +8,9 @@ namespace PromotionEngine.Test
     [TestClass]
     public class ProgramTest
     {
+        /// <summary>
+        /// Sceanario A => 1 A, 1 B, 1 C
+        /// </summary>
         [TestMethod]
         public void GetFinalPrice_ProductA_Count_1_ProductB_Count_1_ProductC_Count_1_Success()
         {
@@ -28,6 +31,31 @@ namespace PromotionEngine.Test
             IEnumerable<IPromotion> orders = new List<IPromotion> { aProduct, bProduct, cProduct };
             decimal finalPrice = Program.GetFinalPrice(orders);
             Assert.AreEqual(100, finalPrice);
+        }
+
+        /// <summary>
+        /// Sceanario A => 1 A, 1 B, 1 C
+        /// </summary>
+        [TestMethod]
+        public void GetFinalPrice_ProductA_Count_1_ProductB_Count_1_ProductC_Count_1_Failure()
+        {
+            AProduct aProduct = new AProduct
+            {
+                ProductsCount = 1
+            };
+
+            BProduct bProduct = new BProduct
+            {
+                ProductsCount = 1
+            };
+            CProduct cProduct = new CProduct
+            {
+                ProductsCount = 1
+            };
+
+            IEnumerable<IPromotion> orders = new List<IPromotion> { aProduct, bProduct, cProduct };
+            decimal finalPrice = Program.GetFinalPrice(orders);
+            Assert.AreNotEqual(0, finalPrice);
         }
     }
 }
