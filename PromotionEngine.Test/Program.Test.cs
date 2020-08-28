113cc63bd456eba7d61aea15262414e44def79d6
Nischal <vkg.arya@gmail.com>
2020-08-28 17:30:57 +0530

    33rd commit - Sceanario 5A, 5B, 1C - Successful

--- PromotionEngine.Test/Program.Test.cs
+++ PromotionEngine.Test/Program.Test.cs
@@ -57,5 +57,30 @@ namespace PromotionEngine.Test
             decimal finalPrice = Program.GetFinalPrice(orders);
             Assert.AreNotEqual(0, finalPrice);
         }
+
+        /// <summary>
+        ///Sceanario B => 5 A, 5 B, 1 C
+        /// </summary>
+        [TestMethod]
+        public void GetFinalPrice_ProductA_Count_5_ProductB_Count_5_ProductC_Count_1_Success()
+        {
+            AProduct aProduct = new AProduct
+            {
+                ProductsCount = 5
+            };
+
+            BProduct bProduct = new BProduct
+            {
+                ProductsCount = 5
+            };
+            CProduct cProduct = new CProduct
+            {
+                ProductsCount = 1
+            };
+
+            IEnumerable<IPromotion> orders = new List<IPromotion> { aProduct, bProduct, cProduct };
+            decimal finalPrice = Program.GetFinalPrice(orders);
+            Assert.AreEqual(370, finalPrice);
+        }
     }
 }
