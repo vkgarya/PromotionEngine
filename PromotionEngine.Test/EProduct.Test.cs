38a017687e45d96581074e3c12e406cc28098a80
Nischal <vkg.arya@gmail.com>
2020-08-28 16:54:58 +0530

    26th commit - This application is ready for extension and adding Product - 'E' with price 100 and having discount of 30%. Putting test case without any code for Product E and it will fail - TDD approach

---
+++ PromotionEngine.Test/EProduct.Test.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PromotionEngine.Services;
+
+namespace PromotionEngine.Test
+{
+    [TestClass]
+    public class EProductTest
+    {
+        [TestMethod]
+        public void GetPrice_Success()
+        {
+            EProduct eProduct = new EProduct
+            {
+                ProductsCount = 1
+            };
+
+            decimal finalPrice = eProduct.GetPrice();
+            Assert.AreEqual(70, finalPrice);
+        }
+    }
+}
