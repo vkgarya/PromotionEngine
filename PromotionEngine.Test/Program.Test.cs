c48845fd365c17b3d49802b5cc118081409ac7d0
Nischal <vkg.arya@gmail.com>
2020-08-28 17:38:29 +0530

    35th commit - Sceanario 3A, 5B, 1C, 1D - Successful Test case

--- PromotionEngine.Test/Program.Test.cs
+++ PromotionEngine.Test/Program.Test.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using PromotionEngine.Interfaces;
+using PromotionEngine.Models;
 using PromotionEngine.Services;
 using System.Collections.Generic;
 
@@ -106,6 +107,35 @@ namespace PromotionEngine.Test
             IEnumerable<IPromotion> orders = new List<IPromotion> { aProduct, bProduct, cProduct };
             decimal finalPrice = Program.GetFinalPrice(orders);
             Assert.AreNotEqual(420, finalPrice);
+        }
+
+        /// <summary>
+        ///Sceanario C => 3 A, 5 B, 1 C, 1 D
+        /// </summary>
+        [TestMethod]
+        public void GetFinalPrice_ProductA_Count_3_ProductB_Count_5_ProductC_Count_1_ProductD_Count_1_Success()
+        {
+            AProduct aProduct = new AProduct
+            {
+                ProductsCount = 3
+            };
+
+            BProduct bProduct = new BProduct
+            {
+                ProductsCount = 5
+            };
+            CDProduct cdProduct = new CDProduct
+            {
+                Products = new List<Product>
+                {
+                    new Product{ ProductName = "C", ProductCount = 1},
+                    new Product{ ProductName = "D", ProductCount = 1}
+                }
+            };
+
+            IEnumerable<IPromotion> orders = new List<IPromotion> { aProduct, bProduct, cdProduct };
+            decimal finalPrice = Program.GetFinalPrice(orders);
+            Assert.AreEqual(280, finalPrice);
         }
     }
 }
