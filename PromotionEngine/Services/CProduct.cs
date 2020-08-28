72c3adb6f672e7910d596b5b79fca49d9b862695
Nischal <vkg.arya@gmail.com>
2020-08-28 16:27:38 +0530

    14th commit - Added code so that the test case can be passed for Product-'C' - TDD approach

---
+++ PromotionEngine/Services/CProduct.cs
@@ -0,0 +1,13 @@
+﻿using PromotionEngine.Interfaces;
+
+namespace PromotionEngine.Services
+{
+    /// <summary>
+    /// CProduct
+    /// </summary>
+    public class CProduct : IPromotion
+    {
+        public int ProductsCount { get; set; }
+        public decimal GetPrice() => 20 * ProductsCount;
+    }
+}
