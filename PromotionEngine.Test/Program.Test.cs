1df34e4667c4805759b0d24ab1985a413fab3ccc
Nischal <vkg.arya@gmail.com>
2020-08-28 17:40:47 +0530

    36th commit - Sceanario 3A, 5B, 1C, 1D - Negative Test case

--- PromotionEngine.Test/Program.Test.cs
+++ PromotionEngine.Test/Program.Test.cs
@@ -137,5 +137,34 @@ namespace PromotionEngine.Test
             decimal finalPrice = Program.GetFinalPrice(orders);
             Assert.AreEqual(280, finalPrice);
         }
+
+        /// <summary>
+        ///Sceanario C => 3 A, 5 B, 1 C, 1 D
+        /// </summary>
+        [TestMethod]
+        public void GetFinalPrice_ProductA_Count_3_ProductB_Count_5_ProductC_Count_1_ProductD_Count_1_Failure()
+        {
+            AProduct aProduct = new AProduct
+            {
+                ProductsCount = 3
+            };
+
+            BProduct bProduct = new BProduct
+            {
+                ProductsCount = 5
+            };
+            CDProduct cdProduct = new CDProduct
+            {
+                Products = new List<Product>
+                {
+                    new Product{ ProductName = "C", ProductCount = 1},
+                    new Product{ ProductName = "D", ProductCount = 1}
+                }
+            };
+
+            IEnumerable<IPromotion> orders = new List<IPromotion> { aProduct, bProduct, cdProduct };
+            decimal finalPrice = Program.GetFinalPrice(orders);
+            Assert.AreNotEqual(335, finalPrice);
+        }
     }
 }
